9ad80b23c7c2686ebe0da7fa484af91631c45660
ykobayashi-techno <ykobayashi@it-nasu.com>
2024-08-19 16:24:55 +0900

    削除 使用してないCounter削除

--- svelte-todo/src/routes/+page.svelte
+++ svelte-todo/src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Counter from "./Counter.svelte";
 	import welcome from "$lib/images/svelte-welcome.webp";
 	import welcome_fallback from "$lib/images/svelte-welcome.png";
 
@@ -108,7 +107,6 @@
 		<button type="submit">Send</button>
 	</form>
 	<p class="system-message">{systemMessage}</p>
-	<Counter />
 </section>
 
 <style>
